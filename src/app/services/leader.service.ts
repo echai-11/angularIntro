7aa21d3d8c68b8484048c98c4b7d01da2c72aadb
Lizzie Chai <lizzie.chai@gmail.com>
2021-09-01 15:55:38 -0500

    add methtod to leader service

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { Leader } from "../shared/leader";
 import { LEADERS } from "../shared/leaders";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LeaderService {
+  constructor() {}
 
-  constructor() { }
-
-   getLeaders(): Leader[] {
+  getLeaders(): Leader[] {
     return LEADERS;
-   }
+  }
+  getLeader(id: string): Leader {
+    return LEADERS.filter((leader) => leader.id === id)[0];
+  }
   getFeaturedLeader(): Leader {
-    return LEADERS.filter((leader) => (leader.featured))[0];
+    return LEADERS.filter((leader) => leader.featured)[0];
   }
 }
