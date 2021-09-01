e035b861421697a3a5b842dff02825f394d2675d
Lizzie Chai <lizzie.chai@gmail.com>
2021-09-01 18:04:33 -0500

    add login dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatListModule } from "@angular/material/list";
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
 
-
-import { AppComponent } from './app.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from "./app.component";
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { MenuComponent } from "./menu/menu.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { LoginComponent } from "./login/login.component";
 
 import { DishService } from "./services/dish.service";
-import { PromotionService } from './services/promotion.service';
-import { LeaderService } from './services/leader.service';
+import { PromotionService } from "./services/promotion.service";
+import { LeaderService } from "./services/leader.service";
 
-import 'hammerjs';
+import "hammerjs";
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import 'hammerjs';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,9 +48,10 @@ import 'hammerjs';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-
+    MatDialogModule,
   ],
+  entryComponents: [LoginComponent],
   providers: [DishService, PromotionService, LeaderService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
